a2ba37b38d5f4b82ad5e5c8fea7a29b5bb08c2f0
Rupert Key <Rupert.Key@GMail.com>
2022-12-04 11:40:11 +0000

    Day 04, part 2.

--- src/aoc-day-04.lua
+++ src/aoc-day-04.lua
@@ -17,28 +17,69 @@ local function part1(line, lineNum)
   end
   
   io.write(string.format("%4d: %3s: ", lineNum, line))
-  local a1, a2, b1, b2 = rex.match(line, [[^(\d+)-(\d+),(\d+)-(\d+)$]])
-  io.write(string.format("(%d-%d) (%d-%d) ", a1, a2, b1, b2))
+  local a = {}
+  local b = {}
+  a[1], a[2], b[1], b[2] = rex.match(line, [[^(\d+)-(\d+),(\d+)-(\d+)$]])
+  io.write(string.format("(%d-%d) (%d-%d) ", a[1], a[2], b[1], b[2]))
   
   -- Don't forget to convert from strings to numbers -- Lua will 'help' and give the wrong answer! eh-hum
-  a1 = tonumber(a1)
-  a2 = tonumber(a2)
-  b1 = tonumber(b1)
-  b2 = tonumber(b2)
+  a[1] = tonumber(a[1])
+  a[2] = tonumber(a[2])
+  b[1] = tonumber(b[1])
+  b[2] = tonumber(b[2])
   
   -- swap if inverted
-  if a2 < a1 then
-    a1, a2 = a2, a1
+  if a[2] < a[1] then
+    a[1], a[2] = a[2], a[1]
   end
-  if b2 < b1 then
-    b1, b2 = b2, b1
+  if b[2] < b[1] then
+    b[1], b[2] = b[2], b[1]
   end
   
   -- check containment
-  if ( a1 >= b1 and a2 <= b2 ) or ( b1 >= a1 and b2 <= a2 ) then
+  if ( a[1] >= b[1] and a[2] <= b[2] ) or ( b[1] >= a[1] and b[2] <= a[2] ) then
     total = total + 1
     
     io.write(" = FULLY CONTAINED")
+  end
+  io.write('\n')
+end
+
+local function part2(line, lineNum)
+  if line == '' then
+    return
+  end
+  
+  io.write(string.format("%4d: %3s: ", lineNum, line))
+  local a = {}
+  local b = {}
+  a[1], a[2], b[1], b[2] = rex.match(line, [[^(\d+)-(\d+),(\d+)-(\d+)$]])
+  io.write(string.format("(%d-%d) (%d-%d) ", a[1], a[2], b[1], b[2]))
+  
+  -- Don't forget to convert from strings to numbers -- Lua will 'help' and give the wrong answer! eh-hum
+  a[1] = tonumber(a[1])
+  a[2] = tonumber(a[2])
+  b[1] = tonumber(b[1])
+  b[2] = tonumber(b[2])
+  
+  -- swap if inverted
+  if a[2] < a[1] then
+    a[1], a[2] = a[2], a[1]
+  end
+  if b[2] < b[1] then
+    b[1], b[2] = b[2], b[1]
+  end
+  
+  -- make 'a' be the lower-starting of the 2 ranges
+  if a[1] > b[1] then
+    a, b = b, a
+  end
+  
+  -- check containment
+  if (a[2] > b[2] and a[1] <= b[2]) or (a[2] >= b[1] and a[2] <= b[2]) then
+    total = total + 1
+    
+    io.write(" = PARTIALLY CONTAINED")
   end
   io.write('\n')
 end
@@ -47,7 +88,7 @@ end
 -- MAIN
 --
 local fNam = "data/04-input.txt"
-Utils.readAndProcessLines(fNam, part1, false, true)
+Utils.readAndProcessLines(fNam, part2, false, true)
 print('total:' .. total)
 
 --luaunit.LuaUnit.run()
