584640b0ab082be653576d3dd14bd5aba5e26dc5
Rupert Key <Rupert.Key@GMail.com>
2022-12-05 22:48:34 +0000

    Stacks starting to be used.

--- src/aoc-day-05.lua
+++ src/aoc-day-05.lua
@@ -2,13 +2,18 @@
 -- Lua for Windows includes an out-of-date LuaUnit so must prepend luaunit path!
 pathOrig = pathOrig or package.path -- workaround for live coding
 package.path = [[./src/?.lua;]] .. pathOrig
-local luaunit = require('luaunit')
+--local luaunit = require('luaunit')
 
 local Utils = require('utils')
+local Stack = require('Stack')
 
 -- http://math2.org/luasearch/rex.html
 require('rex') -- rex_posix
 
+---An array of stacks
+local stacks = {}
+
+---The phase currently in = a function.
 local phase
 
 local function parseInstructions(line, lineNum)
@@ -18,15 +23,26 @@ local function parseInstructions(line, lineNum)
 end
 
 local function parseStacks(line, lineNum)
-  local numMatches = 0
+  io.write("-> ")
+  local stackNum = 1
   for a, b, c in rex.gmatch(line, [[([\[ ])([0-9A-Z ])([\] ]) ?]]) do
-    numMatches = numMatches + 1
-    io.write(string.format("(%s%s%s)", a, b, c))
+    io.write(string.format("%d(%s%s%s) ", stackNum, a, b, c))
     if '1' == b then
-      io.write("Parsing instructions")
+      io.write(" => Parsing instructions")
       phase = parseInstructions
       return
     end
+    -- Still reading stacks
+    if ' ' ~= b then
+      if nil == stacks[stackNum] then
+        --io.write(string.format(" (creating stack %d with %s) ", stackNum, b))
+        stacks[stackNum] = Stack.new({b})
+      else
+        --io.write(string.format(" (adding %s to stack %d %s) ", b, stackNum, tostring(stack)))
+        stacks[stackNum]:push(b) -- TODO: wrong way round => perhaps empty every stack into another to fix ordering?
+      end
+    end
+    stackNum = stackNum + 1
   end
 end
 
